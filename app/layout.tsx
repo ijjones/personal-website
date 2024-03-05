bbe41464bd0f9005b2874939d6c440c8eb975e39
Isaiah Jones <jones.isaiah612@gmail.com>
2024-03-04 22:28:08 -0800

    changed favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,11 @@
 import type { Metadata } from "next";
-import { Montserrat} from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./global.css";
 
 const radio = Montserrat({
   subsets: ["latin"],
   weight: ["400", "600"],
-})
-
+});
 
 export const metadata: Metadata = {
   title: "isaiah",
@@ -23,7 +22,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-     <body className={`${radio.className} min-h-screen pl-0 pr-0 pt-12 pb-2 mx-10 flex justify-center sm:mx-24`}>
+      <body
+        className={`${radio.className} min-h-screen pl-0 pr-0 pt-12 pb-2 mx-10 flex justify-center sm:mx-24`}
+      >
         {children}
       </body>
     </html>
